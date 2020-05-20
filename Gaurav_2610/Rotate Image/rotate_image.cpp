d5f2dbf7cf1b4ec9b8cca44819e95e35e255d309
NoobSolver <mathuranant123@gmail.com>
2020-05-20 13:13:50 +0530

    Solutions added & ss

--- Gaurav_2610/Rotate Image/rotate_image.cpp
+++ Gaurav_2610/Rotate Image/rotate_image.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+    int n=matrix.size();
+for(int i=0;i<matrix.size()/2;i++)
+{
+int x;
+for(int j=0;j<matrix.size()-2*i-1;j++)
+{
+x=matrix[i+j][n-i-1];
+matrix[i+j][n-i-1]=matrix[i][i+j];
+    
+swap(x,matrix[n-i-1][n-(i+j)-1]);
+    
+swap(x,matrix[n-(j+i)-1][i]);
+    
+swap(x,matrix[i][i+j]);
+}
+}    
+        
+    }
+};
--- Gaurav_2610/valid anagram/Valid_anagram.cpp
+++ Gaurav_2610/valid anagram/Valid_anagram.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+          map<char,int>mp;
+        if(s.size()!=t.size())
+            return false;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+            
+            
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            mp[t[i]]--;  
+        }
+        for(auto s:mp)
+        {
+            if(s.second!=0)    
+            return false;
+        }
+        return true;
+    }
+};
