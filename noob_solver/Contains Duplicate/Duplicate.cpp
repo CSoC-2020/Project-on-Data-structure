2646dc7730cffdb495c164918a520d8bfa8c0488
sahillohchab0115 <sahillohchab2797@gmail.com>
2020-05-26 15:14:06 +0530

    .

--- noob_solver/Contains Duplicate/Duplicate.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        set<int>s(nums.begin(),nums.end());
-        return s.size() != nums.size();
-    }
-};
--- noob_solver/Single Number/Single_Number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int x=0;
-        for(int a:nums)
-            x^=a;
-        return x;
-    }
-};
